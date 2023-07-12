c23a46a0a989339140fbd7e52cd84922340ccd4f
dogukan dinler <dogukan12@gmail.com>
2023-07-12 21:13:13 +0300

    yorum bug duzeldi

--- src/main/java/com/Trakya/OrtayaKarisik/Services/YorumService.java
+++ src/main/java/com/Trakya/OrtayaKarisik/Services/YorumService.java
@@ -1,76 +1,83 @@
 package com.Trakya.OrtayaKarisik.Services;
 
-import com.Trakya.OrtayaKarisik.model.Restoran;
-import com.Trakya.OrtayaKarisik.model.User;
-import com.Trakya.OrtayaKarisik.model.Yorum;
 import com.Trakya.OrtayaKarisik.Repository.YorumRepository;
 import com.Trakya.OrtayaKarisik.Requests.CommentCreateRequest;
 import com.Trakya.OrtayaKarisik.Requests.CommentUpdateRequest;
+import com.Trakya.OrtayaKarisik.model.Restoran;
+import com.Trakya.OrtayaKarisik.model.User;
+import com.Trakya.OrtayaKarisik.model.Yorum;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
-
 public class YorumService {
 
-    private static YorumRepository yorumRepository;
-    private  KullaniciService kullaniciService;
-    private   RestoranService restoranService;
+  private static YorumRepository yorumRepository;
+  private KullaniciService kullaniciService;
+  private RestoranService restoranService;
 
-    public YorumService(YorumRepository yorumRepository,KullaniciService kullaniciService,RestoranService restoranService) {
-        this.yorumRepository = yorumRepository;
-        this.kullaniciService = kullaniciService;
-        this.restoranService = restoranService;
+  public YorumService(
+      YorumRepository yorumRepository,
+      KullaniciService kullaniciService,
+      RestoranService restoranService) {
+    this.yorumRepository = yorumRepository;
+    this.kullaniciService = kullaniciService;
+    this.restoranService = restoranService;
+  }
+
+  public static void deleteOneCommentById(Long yorumId) {
+    yorumRepository.deleteById(yorumId);
+  }
+
+  public List<Yorum> getAllCommentsBykullaniciId(Optional<Long> kullaniciId) {
+    if (kullaniciId.isPresent()) {
+      var yorumlar = yorumRepository.findAllByUser_Id(kullaniciId.get());
+      yorumlar.forEach(x -> x.getRestoran().setMenus(null));
+      return yorumlar;
     }
 
-    public static void  deleteOneCommentById(Long yorumId) {
-        yorumRepository.deleteById(yorumId);
+    var yorumlar = yorumRepository.findAll();
+    yorumlar.forEach(x -> x.getRestoran().setMenus(null));
+    return yorumlar;
+  }
 
+  public List<Yorum> getAllCommentsByrestoranId(Optional<Long> restoranId) {
+    if (restoranId.isPresent()) {
+
+      var yorumlar = yorumRepository.findAllByRestoranId(restoranId.get());
+      yorumlar.forEach(x -> x.getRestoran().setMenus(null));
+      return yorumlar;
     }
 
-    public List<Yorum> getAllCommentsBykullaniciId(Optional<Long> kullaniciId) {
-        if(kullaniciId.isPresent()){
-            return yorumRepository.findAllByUser_Id(kullaniciId.get());}
+    var yorumlar = yorumRepository.findAll();
+    yorumlar.forEach(x -> x.getRestoran().setMenus(null));
+    return yorumlar;
+  }
 
-        return yorumRepository.findAll();
-    }
-    public List<Yorum> getAllCommentsByrestoranId(Optional<Long> restoranId) {
-        if(restoranId.isPresent()){
-            return yorumRepository.findAllByRestoranId(restoranId.get());}
+  public Yorum createOneComment(CommentCreateRequest newCommentRequest) {
+    User user = kullaniciService.getOneUser(newCommentRequest.getKullaniciId());
+    Restoran restoran = restoranService.getOneRestoran(newCommentRequest.getRestoranId());
+    if (user != null && restoran != null) {
+      Yorum toSave = new Yorum();
+      toSave.setId(newCommentRequest.getId());
+      toSave.setPuan(newCommentRequest.getPuan());
+      toSave.setUser(user);
+      toSave.setRestoran(restoran);
+      toSave.setYorumTarihi(newCommentRequest.getYorumTarihi());
+      toSave.setYorumIcerigi(newCommentRequest.getYorumIcerigi());
+      return yorumRepository.save(toSave);
+    } else return null;
+  }
 
-        return yorumRepository.findAll();
-    }
+  public Yorum updateOneCommentById(Long yorumId, CommentUpdateRequest updateComment) {
+    Optional<Yorum> yorum = yorumRepository.findById(yorumId);
+    if (yorum.isPresent()) {
+      Yorum toUpdate = yorum.get();
+      toUpdate.setYorumIcerigi(updateComment.getYorumIcerigi());
+      toUpdate.setPuan(updateComment.getPuan());
+      return yorumRepository.save(toUpdate);
 
-    public Yorum createOneComment(CommentCreateRequest newCommentRequest) {
-        User user = kullaniciService.getOneUser(newCommentRequest.getKullaniciId());
-        Restoran restoran = restoranService.getOneRestoran(newCommentRequest.getRestoranId());
-        if(user != null && restoran !=null){
-        Yorum toSave = new Yorum();
-        toSave.setId(newCommentRequest.getId());
-        toSave.setPuan(newCommentRequest.getPuan());
-        toSave.setUser(user);
-        toSave.setRestoran(restoran);
-        toSave.setYorumTarihi(newCommentRequest.getYorumTarihi());
-        toSave.setYorumIcerigi(newCommentRequest.getYorumIcerigi());
-        return yorumRepository.save(toSave);
-        }
-        else
-            return null;
-
-
-    }
-
-    public Yorum updateOneCommentById(Long yorumId, CommentUpdateRequest updateComment) {
-        Optional<Yorum> yorum =yorumRepository.findById(yorumId);
-        if(yorum.isPresent()){
-            Yorum toUpdate= yorum.get();
-            toUpdate.setYorumIcerigi(updateComment.getYorumIcerigi());
-            toUpdate.setPuan(updateComment.getPuan());
-            return yorumRepository.save(toUpdate);
-
-        }
-        else
-            return null;
-    }
+    } else return null;
+  }
 }
--- src/main/java/com/Trakya/OrtayaKarisik/dto/RestoranDTO.java
+++ src/main/java/com/Trakya/OrtayaKarisik/dto/RestoranDTO.java
@@ -1,8 +1,14 @@
 package com.Trakya.OrtayaKarisik.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RestoranDTO {
 
   private Long id;
